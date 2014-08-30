8a2090df28ef3865d46bbd31d01f2d004a88d41b
EricQiu <qiqidone@gmail.com>
2014-08-30 17:44:50 +0800

    Add apis for game score.

---
+++ parent/nex/src/main/java/com/ea/nex/game/GameServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ea.nex.game;
+
+/**
+ * Created by EricQiu on 8/29/2014.
+ */
+public class GameServiceImpl implements GameSevice {
+
+
+
+    public long saveScore(String gameName, long score) {
+        return 0;
+    }
+}
---
+++ parent/nex/src/main/java/com/ea/nex/game/GameSevice.java
@@ -0,0 +1,8 @@
+package com.ea.nex.game;
+
+/**
+ * Created by EricQiu on 8/29/2014.
+ */
+public interface GameSevice {
+    public long saveScore(String gameName, long score);
+}
